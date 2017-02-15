f5c1efa9ab0b1147cf1cfe04ce84d4f547880228
James Borg <James Borg>
2017-02-15 09:07:41 +0100

    Task finished and all test passed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 (function() {
 
-    function ponsify(x, y, z) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
+    function ponsify(lowerLimit, upperLimit, step) {
+		if(lowerLimit == upperLimit)
+		{
+			return lowerLimit;
+		}
+		else if(lowerLimit > upperLimit)
+		{
+			return 0;
+		}
+		else if(lowerLimit > 0 && upperLimit > 0 && step > 0) //the sum does not happen when negative numbers are used
+		{
+			var total = 0;
+			while(lowerLimit <= upperLimit)
+			{
+				total = total + lowerLimit;
+				lowerLimit = lowerLimit + step;
+			}
+			
+			return total;
+		}
     }
     
     // required to export for tests
