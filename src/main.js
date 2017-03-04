35ac23a1f54eaa72e891934272705dbe3d3f8b0f
Daniel <desiradaniel2007@hotmail.com>
2017-03-04 19:36:30 +0100

    replaced for loop by series equation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 (function() {
 
     function ponsify(x, y, z) {
-        var sum = 0;
-				for (var i = x; i <= y; i += z) {
-					sum += i;
+        if (x === y) {
+					return x;
+				} else if (x > y) {
+					return 0;
+				} else {
+					var n = Math.floor((y - x) / z) + 1;
+					return (2 * x + (n - 1) * z) * n / 2;
 				}
-				return sum;
     }
     
     // required to export for tests
