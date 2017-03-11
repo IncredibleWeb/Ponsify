919fa5c44538f9c30afec5820279dc1b42b0eb09
Gasdani <gdfioretto@gmail.com>
2017-03-11 18:10:05 +0100

    First Attempt

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 
     function ponsify(x, y, z) {
         // Add your code here
+    	var c = 0
+    	for (var i = x; i <= y; i=i+z){
+    		c = c + i;
+    	}
+    	
         console.log("Hello world!");
-        return -1;
+        //return -1;
+        return c;
     }
     
     // required to export for tests
