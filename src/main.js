f53b076c13309dfad4c1bed4ffd0f4c52286bbcb
Jamie Spink <jamie@chronoresto.com>
2017-03-23 18:50:12 +0100

    completed ponsify function and ran the test all 3 tests passed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 (function() {
 
     function ponsify(x, y, z) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
-    }
+		var sum = 0;
+	    for (var i = x; i <= y; i += z) {
+	        sum += i;
+	    }
+		return sum;
+	}
     
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
     module.exports = ponsify;
 })();
+
+
