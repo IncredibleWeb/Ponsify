3d58878469ff1d096833f5109f35bea00c973770
Michael Teixeira <mike100042@gmail.com>
2017-07-18 17:48:01 +0100

    Final solution

--- src/main.js
+++ src/main.js
@@ -8,22 +8,13 @@
         // Invalid range
         if( lowerLimit > upperLimit ) return 0;
 
-        var items = [lowerLimit];
+        var total = lowerLimit;
 
         for( var i = lowerLimit; i <= ( upperLimit - step ); i += step ) {
 
             var m = i + step;
-            var n 
-
-            items.push( m );
-
-        }
-
-        var total = 0;
-        if( items.length > 0 ) {
-            for( var i = 0; i < items.length; i++ ) {
-                total += items[i];
-            }
+            
+            total += m; 
         }
 
         return total;
