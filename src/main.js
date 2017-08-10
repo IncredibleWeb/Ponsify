d639618e13de766f6bac1c0345034756443971d6
MagdaKosmala <30873786+MagdaKosmala@users.noreply.github.com>
2017-08-10 19:05:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 
     function ponsify(x, y, z) {
         // Add your code here
-        var sum = 0;
-	var n = (y-x+z)/z;
-	var nInt = parseInt(n);
-	var tmp = 2*x+(nInt-1)*z;
-	sum=tmp*nInt/2;
-	return sum;
+	    var sum = 0;
+	    var n = (y-x+z)/z;
+	    var nInt = parseInt(n);
+	    var tmp = 2*x+(nInt-1)*z;
+	    sum = tmp*nInt/2;
+	    return sum;
     }
     
     // required to export for tests
