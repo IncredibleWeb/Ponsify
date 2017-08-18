4c662eb427f4326799d996efe1daaf03e7675479
Michal Čihák <mcihak@post.cz>
2017-08-18 19:26:18 +0200

    ponsify function implemented in main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 (function() {
-
     function ponsify(x, y, z) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
+        if (x <= y) {
+            // return the sum of integers from lower limit x to upper limit y with step z
+            var sum = 0;
+            while (x <= y) {
+                sum += x;
+                x += z;
+            }
+            return sum;
+        }
+        // return 0 when the upper limit is less than the lower limit
+        else {
+            return 0;
+        }
     }
-    
+
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
