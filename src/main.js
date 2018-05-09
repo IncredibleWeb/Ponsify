be63801ae679895d90c17267be1cca80d4c511b2
hoyoscamila1 <39134750+hoyoscamila1@users.noreply.github.com>
2018-05-09 21:09:20 +0200

    Update main.js
    Camila Ponsify Solution

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@
 
     function ponsify(x, y, z) {
         // Add your code here
+        function ponsify(a, b, c){
+
+        var result = 0;
+  
+        for(i=a; i<=b; i+=c){
+            result += i;  
+        };
+  
+        return(result);
+  
+    };
+
         console.log("Hello world!");
         return -1;
     }
