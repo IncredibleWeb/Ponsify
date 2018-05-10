f798008db34bf298fce9d18b0503249151b80908
ninomirabile <ninomirabile@tiscali.it>
2018-05-10 13:08:32 +0200

    Update 2 after first review

--- src/main.js
+++ src/main.js
@@ -1,31 +1,15 @@
 (function() {
 
     function ponsify(x, y, z) {
-        // Add your code here       
-        if(x>y){
+        // Add your code here   
+        if (x <= y && z > 0) {            
+            var n = Math.floor((y - x + z)/z); //Step
+            return (2*x + (n - 1)*z)*n/2; //Summation
+        }        
+        else {
             return 0;
-        }
-        if(x==y){
-            return x;
-        }
-        if(x<y){
-            var result = 0;
-            var box = [];
-            var counter=0;
-            
-            for (var i = x; i<=y; i++){
-                box.push(i);
-            }
-            for (var j=1; j<box.length; j++){
-                counter++;
-                if(counter == z){
-                    counter =0;
-                    result += box[j];                    
-                }
-            }
-            result +=box[0];
-            return result;
-        }   
+        }        
+        
         //______my solution__________   
         console.log("Hello world!");
         return -1        
