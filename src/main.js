4cea5978279d64c1259dc8a95db499da84d2a249
hoyoscamila1 <39134750+hoyoscamila1@users.noreply.github.com>
2018-05-14 10:27:08 +0200

    Camila Alternate Solution
    Using arithmetic solution instead of loop

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 (function() {
 
     function ponsify(x, y, z) {
-        
         // Add your code here
-        var result = 0;
-  
-        for(i=x; i<=y; i+=z){
-            result += i;  
+        
+        if(x > y){
+        
+            return 0;
+      
+        }else if(x == y){
+      
+            return x;
+        
+        }else{
+        
+        var t = Math.floor(y/z); //how many terms to use in sum
+        return (t/2) * ((2*x) + (t-1) * z); // sum of arithmetic series
+      
         }
-  
-        return(result);
-
+        
         console.log("Hello world!");
         return -1;
     }
