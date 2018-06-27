96c2a70cf2fbfa7fc542cfbad9de101d62d2d653
LAPPINU\yello <samuelborg712@gmail.com>
2018-06-27 02:26:23 +0200

    Commented iterative method

--- src/main.js
+++ src/main.js
@@ -8,6 +8,25 @@
         }
     }
 
+    /*The less expensive but iterative method is the following:
+    function ponsify(x, y, z) {
+      if x > y
+      {
+        return 0;
+      }
+      
+       var j = Math.ceil((y - x) / z);
+      console.log(j);
+      var ans = 0;
+      
+      for (var i = 0; i <= j; i++)
+        {
+          ans += (x + (i*z));
+        }
+      
+      return ans;
+    }*/
+
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
