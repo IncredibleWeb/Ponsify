bc0b07fa98111a6335dd7f574fadfe27248e573f
LAPPINU\yello <samuelborg712@gmail.com>
2018-06-27 02:21:45 +0200

    Fixed syntax error, passed all tests

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 (function () {
 
-        (function ponsify(x, y, z) {
-            if (x <= y) {
-                return (x + ponsify((x + z), y, z));
-            } else {
-                return 0;
-            }
+    function ponsify(x, y, z) {
+        if (x <= y) {
+            return (x + ponsify((x + z), y, z));
+        } else {
+            return 0;
+        }
+    }
 
-            // required to export for tests
-            // DO NOT TOUCH
-            var exports = module.exports;
-            module.exports = ponsify;
-        })();
+    // required to export for tests
+    // DO NOT TOUCH
+    var exports = module.exports;
+    module.exports = ponsify;
+})();
