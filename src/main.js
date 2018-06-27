36482d6397adbcde918051a66b263d90290dd3b5
LAPPINU\yello <samuelborg712@gmail.com>
2018-06-27 02:10:44 +0200

    Initial commit - implemented Ponsify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-(function() {
+(function () {
 
-    function ponsify(x, y, z) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
-    }
-    
-    // required to export for tests
-    // DO NOT TOUCH
-    var exports = module.exports;
-    module.exports = ponsify;
-})();
+        (function ponsify(x, y, z) {
+            if (x <= y) {
+                return (x + ponsify((x + z), y, z));
+            } else {
+                return 0;
+            }
+
+            // required to export for tests
+            // DO NOT TOUCH
+            var exports = module.exports;
+            module.exports = ponsify;
+        })();
