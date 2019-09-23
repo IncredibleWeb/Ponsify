22eadd21f84cf47a1861d8fdd2c71ddf9180a064
Matt <darmanin.matt@gmail.com>
2019-09-24 00:34:17 +0200

    First commit of working script. Currently having issues with Gulp for testing.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 (function() {
 
-    function ponsify(x, y, z) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
-    }
+    function ponsify(lowerLimit, upperLimit, step) {
+        
+        let numberArray = [];
+        let total = 0;
+        
+        if (lowerLimit <= upperLimit) {
+            for (i = lowerLimit; i <= upperLimit; i += step) {
+                numberArray.push(i);
+            }
     
+            /* Using the forEach array method, the total is
+               obtained by adding the value of every integer 
+               in the array to the empty variable 'total' */
+            numberArray.forEach(element => {
+                total += element;
+            });
+        }
+
+        return total;
+    }
+
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
